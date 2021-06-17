6eb7753c9a69de350979b6da6c9034755222b4f4
Bishalshrestha <bsbishalpilla@gmail.com>
2021-06-17 10:36:41 +0545

    ldajslk

--- resources/views/frontend/layouts/partials/footer.blade.php
+++ resources/views/frontend/layouts/partials/footer.blade.php
@@ -1,102 +1,151 @@
-<!-- END SECTION CALL TO ACTION -->
-{{-- <section class="bg_default small_pt small_pb">
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="col-md-8">
-                <div class="text_white cta_section">
-                    <div class="medium_divider d-block d-md-none"></div>
-                    <div class="heading_s1 heading_light">
-                        <h2>KC Group!</h2>
+
+<!-- START FOOTER -->
+
+<footer>
+    <div class="footer-upper">
+        <div class="container">
+            <div class="newsletter text-center">
+                <div class="section-title section-title-white text-center">
+                    <h2>Newsletter Signup</h2>
+                    <div class="section-icon section-icon-white">
+                        <i class="flaticon-diamond"></i>
                     </div>
-                    <p>If you are going to use a passage of embarrassing hidden in the middle of text</p>
+                    <p>Subscribe to our weekly newsletter to get updated on our latest deals</p>
                 </div>
+                <form>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="search">
+                        <a href="#"><span class="search_btn"><i class="fa fa-paper-plane" aria-hidden="true"></i> Sign Up</span></a>
+                    </div>
+                </form>
             </div>
-            <div class="col-md-4">
-                <div class="text-md-right">
-                    <a href="#" class="btn btn-outline-white rounded-0">Get Started</a>
+            <div class="footer-links">
+                <div class="row">
+                    <div class="col-md-3 col-sm-12">
+                        <div class="footer-about footer-margin">
+                            <div class="about-logo">
+                                <img src="images/Yatra-white.png" alt="Image">
+                            </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt.</p>
+                            <div class="about-location">
+                                <ul>
+                                    <li><i class="flaticon-maps-and-flags" aria-hidden="true"></i> Location</li>
+                                    <li><i class="flaticon-phone-call"></i> (012)-345-6789</li>
+                                    <li><i class="flaticon-mail"></i> <a
+                                            href="https://cyclonethemes.com/cdn-cgi/l/email-protection"
+                                            class="__cf_email__"
+                                            data-cfemail="bcc8d3c9ced2c8ceddcad9d0fcc8d9cfc8d1ddd5d092dfd3d1">[email&#160;protected]</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="footer-social-links">
+                                <ul>
+                                    <li class="social-icon"><a href="#"><i class="fa fa-facebook"
+                                                                           aria-hidden="true"></i></a></li>
+                                    <li class="social-icon"><a href="#"><i class="fa fa-instagram"
+                                                                           aria-hidden="true"></i></a></li>
+                                    <li class="social-icon"><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                    </li>
+                                    <li class="social-icon"><a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+                                    </li>
+                                    <li class="social-icon"><a href="#"><i class="fa fa-google" aria-hidden="true"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-4">
+                        <div class="footer-links-list footer-margin">
+                            <h3>Expedition</h3>
+                            <ul>
+                                <li><a href="#">Cyclades <i class="fa fa-angle-right" aria-hidden="true"></i></a></li>
+                                <li><a href="#">North Ionian <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+                                </li>
+                                <li><a href="#">Sporades <i class="fa fa-angle-right" aria-hidden="true"></i></a></li>
+                                <li><a href="#">View all Destinations <i class="fa fa-angle-right"
+                                                                         aria-hidden="true"></i></a></li>
+                                <li><a href="#">View all Yatchs <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+                                </li>
+                                <li><a href="#">View all Cruises <i class="fa fa-angle-right"
+                                                                    aria-hidden="true"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-4">
+                        <div class="footer-links-list footer-margin">
+                            <h3>Trekking</h3>
+                            <ul>
+                                <li><a href="#">Cyclades <i class="fa fa-angle-right" aria-hidden="true"></i></a></li>
+                                <li><a href="#">North Ionian <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+                                </li>
+                                <li><a href="#">Sporades <i class="fa fa-angle-right" aria-hidden="true"></i></a></li>
+                                <li><a href="#">View all Destinations <i class="fa fa-angle-right"
+                                                                         aria-hidden="true"></i></a></li>
+                                <li><a href="#">View all Yatchs <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+                                </li>
+                                <li><a href="#">View all Cruises <i class="fa fa-angle-right"
+                                                                    aria-hidden="true"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-4">
+                        <div class="footer-links-list">
+                            <div class="footer-instagram">
+                                <h3>Instagram</h3>
+                                <ul>
+                                    <li><img src="images/insta1.jpg" alt="Image"></li>
+                                    <li><img src="images/insta2.jpg" alt="Image"></li>
+                                    <li><img src="images/insta3.jpg" alt="Image"></li>
+                                    <li><img src="images/insta4.jpg" alt="Image"></li>
+                                    <li><img src="images/insta5.jpg" alt="Image"></li>
+                                    <li><img src="images/insta6.jpg" alt="Image"></li>
+                                    <li><img src="images/insta7.jpg" alt="Image"></li>
+                                    <li><img src="images/insta8.jpg" alt="Image"></li>
+                                    <li><img src="images/insta9.jpg" alt="Image"></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="medium_divider d-block d-md-none"></div>
             </div>
         </div>
     </div>
-</section> --}}
-<!-- END SECTION CALL TO ACTION -->
-<!-- START FOOTER -->
-<footer class="bg_blue_dark footer_dark">
-    <div class="top_footer">
+    <div class="copyright">
         <div class="container">
             <div class="row">
-                <div class="col-lg-4 col-md-6 col-6 mb-4 mb-lg-0">
-                    <div >
-                        <a href="{{route('homepage')}}">
-                            <img class="footer_logo" class="bg-white" src="{{asset('assets/images/kclogo.png')}}">
-                        </a> 
+                <div class="col-xs-6">
+                    <div class="copyright-content">
+                        <p>2020 <i class="fa fa-copyright" aria-hidden="true"></i> Himalayan Shepherd
+                        </p>
                     </div>
-                    <!-- <p>Phasellus blandit massa enim. elit id varius nunc. Lorem ipsum dolor sit amet, consectetur industry.</p> -->
-                    <ul >
-                        <div class="blog_text">
-                                <h4>KC Group</h4>
-                            </div>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</p>
-                        
-                    </ul>
                 </div>
-                <div class="col-lg-4 col-md-6 col-6 mb-4 mb-lg-0">
-                    <h6 class="widget_title">Quick Links</h6>
-                    <ul class="list_none widget_links links_style1">
-                        <li><a href="{{route('homepage')}}">Home</a></li>
-                            <li><a href="{{url('about')}}">About</a></li>
-                        <li><a href="{{url('gallery')}}">Gallery</a></li>
-                        <li><a href="{{url('contact')}}">Contact</a></li>
-
-                        <!--@foreach(menus() as $menu)-->
-                        <!--    <li><a href="{{url($menu->url)}}">{{$menu->name}}</a></li>-->
-                        <!--@endforeach-->
-                    </ul>
+                <div class="col-xs-6">
+                    <div class="payment-content">
+                        <ul>
+                            <li>We Accept</li>
+                            <li>
+                                <img src="images/payment1.png" alt="Image">
+                            </li>
+                            <li>
+                                <img src="images/payment2.png" alt="Image">
+                            </li>
+                            <li>
+                                <img src="images/payment3.png" alt="Image">
+                            </li>
+                            <li>
+                                <img src="images/payment4.png" alt="Image">
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                
-                <div class="col-lg-4 col-md-6 col-6 mb-4 mb-lg-0">
-                <h6 class="widget_title">Contact Us</h6>
-                <ul class="list_none social_icons social_white social_style1">
-                        @if(setting('facebook') !=null)
-                            <li><a href="{{setting('facebook')}}" class="socialfacebook"><i class="ion-social-facebook"></i></a></li>
-                        @endif
-                        @if(setting('twitter') !=null)
-                            <li><a href="{{setting('twitter')}}" class="socialtwitter"><i class="ion-social-twitter"></i></a></li>
-                        @endif
-                        @if(setting('linkedin') !=null)
-                            <li><a href="{{setting('linkedin')}}" class="sociallinkedin"><i class="ion-social-linkedin"></i></a></li>
-                        @endif
-                    </ul>
-
-                    <ul class="mt-3 contact_info contact_info_light list_none">
-                        <li>
-                            <i class="fa fa-map-marker-alt "></i>
-                            <address>{{setting('address')}}</address>
-                        </li>
-                        <li>
-                            <i class="fa fa-envelope"></i>
-                            <a href="mailto:{{setting('email')}}">{{setting('email')}}</a>
-                        </li>
-                        <li>
-                            <i class="fa fa-mobile-alt"></i>
-                                <a href="tel:{{setting('phone')}}">{{setting('phone')}}</a>
-                        </li>
-                    </ul>
-                </div>
-              
-            </div>
-        </div>
-    </div>
-    <div class="bottom_footer bg_blue_dark2">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-md-6">
-                    <p class="copyright m-md-0 text-center text-md-left">© {{date('Y')}} All Rights Reserved by KC Group.</p>
-                </div>
-               
             </div>
         </div>
     </div>
 </footer>
+
+
+<div id="back-to-top">
+    <a href="#"></a>
+</div>
 <!-- END FOOTER -->
